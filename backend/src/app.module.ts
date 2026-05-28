77d7b5d42d096be70d9430d8ab9188bec4bd8343
Diego Jesus DV. Valencia Garcia <dvalencia@laportadasac.local>
2026-05-28 11:29:55 -0500

    conexion postgres render

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,28 +3,32 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UsuariosModule } from './usuarios/usuarios.module';
-import { EquiposModule } from './equipos/equipos.module';
+import { PozosModule } from './pozos/pozos.module';
 import { LecturasModule } from './lecturas/lecturas.module';
 
 @Module({
   imports: [
+
     TypeOrmModule.forRoot({
       type: 'postgres',
+    
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+    
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
 
-      host: 'localhost',
-      port: 5432,
-
-      username: 'postgres',
-      password: '123456',
-
-      database: 'sistema_control_electrico',
+      database: process.env.DB_NAME,
 
       autoLoadEntities: true,
+      dropSchema: true,
       synchronize: true,
     }),
+
     UsuariosModule,
-    EquiposModule,
+    PozosModule,
     LecturasModule,
   ],
 
